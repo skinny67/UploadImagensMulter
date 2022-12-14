2f942500f510c882f3118973e9ea195a5c29d214
Pablo Brunetti <pablo.brunetti21@example.com>
2022-12-14 17:33:53 -0400

    commit geral

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
-    res.send('<h1>Olá Mundo!</h1>')
-})
+    res.render('index.ejs',{})
+});
+
+app.get('/usuarios', (req, res)=>{
+    res.render('usuarios.ejs',{ usuarios:[
+        {nome:'Pablo',email:'pablo@email.com'},
+        {nome:'Pablo',email:'pablo@email.com'},
+        {nome:'Pablo',email:'pablo@email.com'},
+        {nome:'Pablo',email:'pablo@email.com'},
+    ]});
+});
+
 
 app.listen(3000, ()=>{
     console.log('Sistma ON!')
