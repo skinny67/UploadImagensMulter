4865882495f0364fd4184888ae559b5ffff3c275
Pablo Brunetti <pablo.brunetti21@example.com>
2022-12-14 16:09:57 -0400

    Primeira rota get criada

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', ()=>{
+    console.log('Realizando o acesso ao servidor!')
+})
+
 app.listen(3000, ()=>{
     console.log('Sistma ON!')
 })
