202344b97e76dfb5ae4afc580903c49116e0a60f
Pablo Brunetti <pablo.brunetti21@example.com>
2022-12-14 21:15:21 -0400

    novo commit geral

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 const Usuario = require('./models/usuario');
-mongoose.set('strictQuery', false);
 
+// Conexão com o banco de dados e porta da aplicação
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.CONNECTIONSTRING,
     {   
         // useNewUrlParser: true, 
@@ -26,27 +27,39 @@ app.on('PRONTO', ()=> {
         console.log('Acessar http://localhost:3000');
     });
 });
+// FIM Conexão com o banco de dados e porta da aplicação
 
+
+// Configurações da aplicação
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+//FIM Configurações da aplicação
 
+//Rotas Da aplicação
+
+// Rota Inicial
 app.get('/', (req, res)=>{
-    res.render('index.ejs',{})
+    Usuario.find({}).exec((err, docs)=>{
+        res.render('index.ejs',{ Usuarios:docs})
+    })
 });
+// FIM Rota Inicial
 
-// app.get('/usuarios', (req, res)=>{
-//     res.render('usuarios.ejs',{ usuarios:[
-//         {nome:'Pablo',email:'pablo@email.com'},
-//         {nome:'Pablo',email:'pablo@email.com'},
-//         {nome:'Pablo',email:'pablo@email.com'},
-//         {nome:'Pablo',email:'pablo@email.com'},
-//     ]});
-// });
 
+// Botão de pesquisa
+app.post('/', (req, res)=>{
+    Usuario.find({nome: new RegExp(req.body.txtPesquisa,'gi')}).exec((err, docs)=>{
+        res.render('index.ejs',{Usuario:docs})
+    });
+});
+// FIM Botão de pesquisa
+
+
+// Rota de adicionar
 app.get('/add', (req, res)=>{
     res.render('adiciona.ejs')
 });
@@ -66,3 +79,40 @@ app.post('/add', (req, res)=>{
         }
     });
 });
+//FIM Rota de adicionar
+
+// Rota delete
+app.get('/del/:id', (req, res)=>{
+    Usuario.findByIdAndDelete(req.params.id, (err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect('/')
+        }
+    })
+});
+// FIM Rota delete
+
+// Rota editar
+app.get('/edit/:id', (req, res)=>{
+    Usuario.findById(req.params.id,(err, docs)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.render('edita.ejs',{Usuario: docs})
+        }
+    })
+});
+
+app.post('/edit/:id', (req, res)=>{
+    Usuario.findByIdAndUpdate(req.params.id,{ 
+        nome:req.body.txtNome,
+        email:req.body.txtEmail,
+        senha:req.body.txtSenha,
+        foto:req.body.txtFoto
+    },(err, docs)=>{
+        res.redirect('/')
+    }
+    )
+});
+//FIM Rota editar
